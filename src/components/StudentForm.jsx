6c46f798003d162153c14069d029ce0320a8eb30
Samyak Khatua <samyakkhatua@duck.com>
2023-01-06 02:50:06 +0530

    Styling added to form

--- src/components/StudentForm.jsx
+++ src/components/StudentForm.jsx
@@ -1,51 +1,63 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
-const StudentForm = ({addStudent}) => {
-    const [studentInfo, setStudentInfo] = useState({
-        name: "",
-        roll: "",
-    })
+const StudentForm = ({ addStudent }) => {
+  const [studentInfo, setStudentInfo] = useState({
+    name: "",
+    roll: "",
+  });
 
-    const handleChange = (e) => {
-        const {name, value} = e.target;
+  const handleChange = (e) => {
+    const { name, value } = e.target;
 
-        setStudentInfo((preValue) => {
-            return {...preValue, [name]: value}
-        })
-    }
+    setStudentInfo((preValue) => {
+      return { ...preValue, [name]: value };
+    });
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(studentInfo);
-        
-        addStudent(studentInfo);
-        setStudentInfo({name: "", roll:"",})
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(studentInfo);
 
-    }
+    addStudent(studentInfo);
+    setStudentInfo({ name: "", roll: "" });
+  };
 
   return (
-    <div>
-        <form action="" onSubmit={handleSubmit}>
-            <input 
-                name='name' 
-                value={studentInfo.name} 
-                onChange={handleChange} 
-                type="text"
-                placeholder='name'
-            />
-            <input 
-                name='roll' 
-                value={studentInfo.roll} 
-                onChange={handleChange} 
-                type="text"
-                placeholder='roll'
-            />
+    <div className="mb-10 ">
+      <form className="flex gap-4 " onSubmit={handleSubmit}>
+        <div class="mb-2">
+          <input
+            name="name"
+            value={studentInfo.name}
+            onChange={handleChange}
+            type="text"
+            placeholder="Student Name"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            required
+          />
+        </div>
 
-            <button>Submit</button>
-            
-        </form>
+        <div class="mb-4">
+          <input
+            name="roll"
+            value={studentInfo.roll}
+            onChange={handleChange}
+            type="text"
+            placeholder="Roll Number"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            required
+          />
+        </div>
+
+        <button
+          type="submit"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-4 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 "
+        >
+          Submit
+        </button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default StudentForm
+export default StudentForm;
