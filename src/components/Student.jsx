103ecd07b00447e0fcbc0bea01164c5e2b8b2c00
Samyak Khatua <samyakkhatua@duck.com>
2023-01-06 01:49:45 +0530

    checkin & checkout one way option

--- src/components/Student.jsx
+++ src/components/Student.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 
 const Student = ({ student, handleCheckIn, handleCheckOut }) => {
-
   const handleInClick = (e) => {
     e.preventDefault(e.currentTarget.id);
     handleCheckIn(e.currentTarget.id);
   };
-
 
   const handleOutClick = (e) => {
     e.preventDefault(e.currentTarget.id);
@@ -14,18 +12,16 @@ const Student = ({ student, handleCheckIn, handleCheckOut }) => {
     handleCheckOut(e.currentTarget.id);
   };
 
-  var today = new Date()
-  let currTime = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds()
-
+  var today = new Date();
+  let currTime =
+    today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 
   return (
-    
     <tr class="w-full bg-white text-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-
       <td class="px-6 py-4 text-base font-semibold ">{student.roll}</td>
 
-        {/* name */}
-      <td className="px-6 py-4" >{student.name}</td>
+      {/* name */}
+      <td className="px-6 py-4">{student.name}</td>
 
       <td class="px-6 py-4">
         <div class="flex items-center">
@@ -38,24 +34,29 @@ const Student = ({ student, handleCheckIn, handleCheckOut }) => {
 
       <td class="px-6 py-4">{student.checkout}</td>
 
-    {/* action buttons */}
+      {/* action buttons */}
       <td class="px-6 py-4 flex gap-2">
-        <button
-        id={student.id}
-          type="button"
-          onClick={handleInClick}
-          className="px-3 py-2 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        >
-          CheckIn
-        </button>
-        <button
-        id={student.id}
-          type="button"
-          onClick={handleOutClick}
-          class="px-3 py-2 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        >
-          CheckOut
-        </button>
+        {student.status == false && (
+          <button
+            id={student.id}
+            type="button"
+            onClick={handleInClick}
+            className="px-3 py-2 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            CheckIn
+          </button>
+        )}
+
+        {student.status == true && (
+          <button
+            id={student.id}
+            type="button"
+            onClick={handleOutClick}
+            class="px-3 py-2 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            CheckOut
+          </button>
+        )}
       </td>
     </tr>
   );
--- src/components/StudentList.jsx
+++ src/components/StudentList.jsx
@@ -7,18 +7,6 @@ const StudentList = ({ studentList, handleCheckIn, handleCheckOut }) => {
       <table class="w-[100%] text-sm text-left text-gray-500 dark:text-gray-400">
         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
           <tr className="w-full">
-            {/* <th scope="col" class="p-4">
-              <div class="flex items-center">
-                <input
-                  id="checkbox-all-search"
-                  type="checkbox"
-                  class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                />
-                <label for="checkbox-all-search" class="sr-only">
-                  checkbox
-                </label>
-              </div>
-            </th> */}
             <th scope="col" class="px-6 py-3">
               Roll No.
             </th>
